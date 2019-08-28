8e5e68fac785dcc9b8024a907a55b5f6789f449b
Artem Zaitsev <artz1994@mail.ru>
2019-08-29 00:33:03 +0500

    a

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import collections
 def echo(request):
     return web.Response(text=json.dumps(request))
 
+web.run_app(app, port=os.getenv('PORT', 8000))
 app = web.Application()
 app.add_routes([
     web.get('/', echo)
