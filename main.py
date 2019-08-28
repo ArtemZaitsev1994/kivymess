c7f864f77427f105e769927557ca9af718c11a2b
Artem Zaitsev <artz1994@mail.ru>
2019-08-29 00:34:47 +0500

    a

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from aiohttp import web
 import json
 from datetime import datetime
 import collections
+import os
 
 
 def echo(request):
     return web.Response(text=json.dumps(request))
 
-web.run_app(app, port=os.getenv('PORT', 8000))
 app = web.Application()
+web.run_app(app, port=os.getenv('PORT', 8000))
 app.add_routes([
     web.get('/', echo)
 ])
