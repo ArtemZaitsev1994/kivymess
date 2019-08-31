a6c6f7f3a3cd678b9c3d39731f4dde0875578ef1
Artem Zaitsev <artz1994@mail.ru>
2019-08-31 13:52:15 +0500

    a

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import MySQLdb
 
 async def all_news(request):
-	print(2)
+    print(2)
     result = request.transport.get_extra_info('peername')
     with DataBase() as db:
         print(3)
@@ -72,5 +72,5 @@ class DataBase:
 
 
 if __name__ == '__main__':
-	print(1)
+    print(1)
     web.run_app(app)
